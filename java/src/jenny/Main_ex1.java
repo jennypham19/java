7e9a569667585896bce776a1836da649ab3ea5d5
jennypham19 <81556663+jennypham19@users.noreply.github.com>
2021-04-25 21:58:29 +0700

    Add files via upload

--- java/src/jenny/Main_ex1.java
+++ java/src/jenny/Main_ex1.java
@@ -1,10 +1,9 @@
 package jenny;
 
 public class Main_ex1 {
-
 	public static void main(String[] args) {
-	
-
+		ex2 myEx2 = new ex2 (null, null, 0, 0, 0);
+		myEx2.display();
 	}
 
 }
--- java/src/jenny/ex2.java
+++ java/src/jenny/ex2.java
@@ -1,11 +1,44 @@
 package jenny;
 
+import java.util.Scanner;
+
 public class ex2 {
-	private String Name;
-	private String Class_Name;
-	public ex2(String Name,String Class_Name) {
-		this.Name = Name;
-		this.Class_Name = Class_Name;
+	private static String Name;
+	private static String Class_Name;
+	private static float C;
+	private static float PHP;
+	private static float diemTB;
+	private String xepLoai;
+	public void display(){
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.println("Enter your name: ");
+			Name = input.nextLine();
+			System.out.println("Enter your class: ");
+			Class_Name = input.nextLine();
+			System.out.println("Enter your C mark: ");
+			C = input.nextFloat();
+			System.out.println("Enter your PHP mark: ");
+			PHP = input.nextFloat();
+		}
+		diemTB = (C + PHP)/2;
+		System.out.println("Diem trung binh cua " + getName() + " la: " + getDiemTB());
+		if(diemTB>=8) {
+			xepLoai = "Gioi";
+		}else if (diemTB>=6){
+			xepLoai = "Kha";
+		}else if (diemTB>=5) {
+			xepLoai = "TB";
+		}else {
+			xepLoai = "Khong dat";
+		}
+		System.out.println("Ket qua xep loai hoc tap cua " + getName()  + " la: " + getXepLoai());
+	}
+	public ex2(String Name,String Class_Name,float C,float PHP,float diemTB) {
+		ex2.Name = Name;
+		ex2.Class_Name = Class_Name;
+		ex2.C = C;
+		ex2.PHP = PHP;
+		ex2.diemTB = diemTB;
 	}
 	public String getName() {
 		return Name;
@@ -19,7 +52,31 @@ public class ex2 {
 	public void setClass_Name(String class_Name) {
 		Class_Name = class_Name;
 	}
-	
+	public String getXepLoai() {
+		return xepLoai;
+	}
+	public void setXepLoai(String xepLoai) {
+		this.xepLoai = xepLoai;
+	}
+	public float getDiemTB() {
+		return diemTB;
+	}
+	public void setDiemTB(float diemTB) {
+		diemTB = diemTB;
+	}
+	public float getC() {
+		return C;
+	}
+	public void setC(float c) {
+		C = c;
+	}
+	public float getPHP() {
+		return PHP;
+	}
+	public void setPHP(float pHP) {
+		PHP = pHP;
+	}
+
 	
 
 }
